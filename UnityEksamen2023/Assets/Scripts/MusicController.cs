da4da6efed3e2d9d37fd79ce3cf31520cdb22caf
Wyctex <129751536+Wyctex@users.noreply.github.com>
2023-04-05 14:40:05 +0200

    dd

--- UnityEksamen2023/Assets/Scripts/MusicController.cs
+++ UnityEksamen2023/Assets/Scripts/MusicController.cs
@@ -1,14 +1,44 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MusicController : MonoBehaviour
 {
-    public Slider volumeSlider;
+    public static MusicController instance;
+
     public AudioSource musicSource;
+    public Slider volumeSlider;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
     void Start()
     {
-        volumeSlider.onValueChanged.AddListener(OnVolumeChanged);
+        SceneManager.sceneLoaded += OnSceneLoaded;
+
+        if (SceneManager.GetActiveScene().name == "OptionsMenu")
+        {
+            volumeSlider = GameObject.Find("SliderForMusicAdjsut").GetComponent<Slider>();
+            volumeSlider.onValueChanged.AddListener(OnVolumeChanged);
+        }
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name != "OptionsMenu")
+        {
+            musicSource.Play();
+        }
     }
 
     void OnVolumeChanged(float volume)
