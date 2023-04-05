8d5f08761b7b8feaaa6280f39e4234b46a554108
Wyctex <129751536+Wyctex@users.noreply.github.com>
2023-04-05 14:51:23 +0200

    2131

--- UnityEksamen2023/Assets/Scripts/CharacterControllerScript.cs
+++ UnityEksamen2023/Assets/Scripts/CharacterControllerScript.cs
@@ -4,48 +4,15 @@ using UnityEngine;
 
 public class CharacterControllerScript : MonoBehaviour
 {
-
-    [Header("References")]
-    public float moveSpeed;
-
-    public Transform orientation;
-
-    float horizontalInput;
-    float verticalInput;
-
-    Vector3 moveDirection;
-
-    Rigidbody rb;
-
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        rb.freezeRotation = true;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        MyInput();
-    }
-
-    private void FixedUpdate()
-    {
-        MovePlayer();
-    }
-
-    private void MyInput()
-    {
-        horizontalInput = Input.GetAxisRaw("Horizontal");
-        verticalInput = Input.GetAxisRaw("Vertical");
-    }
-
-    private void MovePlayer()
-    {
-        // Calculate movement direction
-        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
-
-        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+        
     }
 }
