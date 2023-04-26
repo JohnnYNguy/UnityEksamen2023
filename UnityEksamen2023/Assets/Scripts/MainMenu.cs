eb5fd7c290de0b5809fcb2fc831f3255703b2d98
Faisal Omar <93327868+faom002@users.noreply.github.com>
2023-04-26 15:38:13 +0200

    uopdate game menu

--- UnityEksamen2023/Assets/Scripts/MainMenu.cs
+++ UnityEksamen2023/Assets/Scripts/MainMenu.cs
@@ -1,17 +1,26 @@
+// Importing the required namespaces
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
+// Defining the MainMenu class
 public class MainMenu : MonoBehaviour
 {
-   public void PlayGame()
-   {
-    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
-   }
+    // Public function to start the game
+    public void PlayGame()
+    {
+        // Load the next scene in the build index
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
 
-   public void ExitGame()
-   {
-    Debug.Log("You have exited the game :)");
-    Application.Quit();
-   }
+    // Public function to exit the game
+    public void ExitGame()
+    {
+        // Log a message to the console
+        Debug.Log("You have exited the game :)");
+
+        // Quit the application, this will only work if the game is exported as exe 
+        Application.Quit();
+    }
 }
--- UnityEksamen2023/Assets/Scripts/MusicController.cs
+++ UnityEksamen2023/Assets/Scripts/MusicController.cs
@@ -1,64 +1,84 @@
+// Importing necessary Unity libraries which comes with autcompletion when writing code thanks to visual :)
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
+// calling our class MusicController, inheriting from MonoBehaviour  
 public class MusicController : MonoBehaviour
 {
+    // Defining instance variable as static to make it a singleton
     public static MusicController instance;
 
+    // Creating a AudioSource variable to hold music audio
     public AudioSource musicSource;
+
+    // Creating float variable to hold volume value
     public float volumeSlider;
 
-    private Slider sliderComponent; // added variable to reference the Slider component
+    // Adding a Slider variable to reference the Slider component
+    private Slider sliderComponent;
 
+    // Awake method is called when script instance is being loaded
     void Awake()
     {
+        // Check if instance is null, if so set instance as this and then continue between scenes
         if (instance == null)
         {
             instance = this;
             DontDestroyOnLoad(gameObject);
         }
-        else
+        else // If instance already exists, destroy this instance
         {
             Destroy(gameObject);
         }
     }
 
+    // Start method is called on the first frame the script is active so only activate once after game starts
     void Start()
     {
+        // Add OnSceneLoaded method to sceneLoaded event
         SceneManager.sceneLoaded += OnSceneLoaded;
 
+        // If the active scene is the main menu
         if (SceneManager.GetActiveScene().name == "MainMenu")
         {
+            // Find the Slider component in the OptionsMenu canvas and set its value, to get to slider we had to set filepath like this "Canvas/OptionsMenu/Slider"
             sliderComponent = GameObject.Find("Canvas/OptionsMenu/Slider").GetComponent<Slider>();
             sliderComponent.onValueChanged.AddListener(OnVolumeChanged);
             sliderComponent.value = volumeSlider; // setting the Slider value using the float variable
         }
     }
 
+    // OnSceneLoaded method is called after a new scene has finished loading
     void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
+        // If the loaded scene is the main menu
         if (scene.name == "MainMenu")
         {
+            // Find the Slider component in the OptionsMenu canvas and set its value, to get to slider we had to set filepath like this "Canvas/OptionsMenu/Slider"
             sliderComponent = GameObject.Find("Canvas/OptionsMenu/Slider").GetComponent<Slider>();
             sliderComponent.onValueChanged.AddListener(OnVolumeChanged);
             sliderComponent.value = volumeSlider; // setting the Slider value using the float variable
         }
-        else
+        else // Otherwise, play the music
         {
             musicSource.Play();
         }
     }
 
+    // OnVolumeChanged method is called when the slider value for volume changes
     void OnVolumeChanged(float volume)
     {
+        // a debugger for checkiing if it does actually change the volumes value by using slider
         Debug.Log("Volume changed to: " + volume);
         volumeSlider = volume;
         musicSource.volume = volumeSlider;
     }
 
+    // OnDestroy method is called when the script is being destroyed
     void OnDestroy()
     {
+        // If the Slider component exists, remove the OnVolumeChanged event listener and set the volumeSlider value
         if (sliderComponent != null)
         {
             sliderComponent.onValueChanged.RemoveListener(OnVolumeChanged);
